a6ae5b4e3528210990ca34045fc347d523f556fc
George <gorbel01@mail.birkbeck.ac.uk>
2014-11-24 17:50:59 +0000

    Added Concurrency1 work from class.

---
+++ Week9/src/Counter.java
@@ -0,0 +1,9 @@
+public class Counter {
+    private int n = 0;
+    public synchronized void increase() {
+        n++;
+    }
+    public int getCount() {
+        return n;
+    }
+}
---
+++ Week9/src/ResponsiveUI.java
@@ -0,0 +1,13 @@
+public class ResponsiveUI
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; 
+        System.out.println("Enter the Duration (in ms) of task: );
+    }
+    
+    public void getLength()
+    {
+        
+    }
+}
